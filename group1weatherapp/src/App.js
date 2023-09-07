f0084e3519970ddcf96ac219f6575b7b3a702762
AyaMouawi <ayamouawi8@gmail.com>
2023-09-07 14:45:11 +0300

    fetching from api

--- group1weatherapp/src/App.js
+++ group1weatherapp/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import WeatherHour from './Comps/WeatherHour';
@@ -6,36 +5,50 @@ import CurrentWeather from './Comps/CurrentWeather';
 
 function App() {
   const [weatherData, setWeatherData] = useState([]);
+  const currentDate = new Date();
+  const [city, setCity] = useState('lebanon'); // Default city (you can change this)
+
+  const handleCityChange = (newCity) => {
+    setCity(newCity);
+  };
 
   useEffect(() => {
     const apiKey = '49dbc11a976fd95d5d464739a2a668e8';
-    const city = 'lebanon'; // to be changed
-    
-    // Fetch weather data for the next 5 days in 3-hour intervals
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+
+    // Fetch weather data for the current day with 3-hour intervals based on user-entered city
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`)
       .then((response) => response.json())
       .then((data) => {
-        // Extract data for every 3 hours and store it in weatherData state
+        // Filter the data for every 3 hours
         const filteredData = data.list.filter((item, index) => index % 8 === 0); // 8 * 3 hours = 24 hours
+
+        // Update the weatherData state with the filtered data
         setWeatherData(filteredData);
       })
       .catch((error) => {
         console.error('Error fetching weather data:', error);
       });
-  }, []);
+  }, [city]);
 
   return (
     <div>
-      <CurrentWeather />
+      <CurrentWeather onCityChange={handleCityChange} />
       <div className="main-division">
-        {weatherData.map((data, index) => (
-          <WeatherHour
-            key={index}
-            time={data.dt_txt}
-            iconSrc={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-            temperature={`${data.main.temp} °C`}
-          />
-        ))}
+        {weatherData.map((data, index) => {
+          // Calculate the time for each WeatherHour based on the current time and index
+          const time = new Date(currentDate);
+          time.setHours(currentDate.getHours() + (3 * index));
+          const formattedTime = `${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}`;
+
+          return (
+            <WeatherHour
+              key={index}
+              time={formattedTime} // Updated time format
+              iconSrc={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+              temperature={`${data.main.temp} °C`}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- group1weatherapp/src/Comps/CurrentWeather.js
+++ group1weatherapp/src/Comps/CurrentWeather.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Weather.css';
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ onCityChange }) => {
   const [weatherData, setWeatherData] = useState({
     humidity: '',
     wind: '',
@@ -9,54 +9,68 @@ const CurrentWeather = () => {
     currentTemperature: '',
     tempDesc: '',
   });
+  const [city, setCity] = useState('lebanon'); //Default as lebanon
 
-  useEffect(() => {
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const fetchWeatherData = () => {
     const apiKey = '49dbc11a976fd95d5d464739a2a668e8';
-    const city = 'lebanon'; // to be changed 
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    // Fetch current weather data in Celsius based on user-entered city
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
       .then((response) => response.json())
       .then((data) => {
-        
         // Update the weatherData state with the fetched data
         setWeatherData({
           humidity: data.main.humidity,
           wind: data.wind.speed,
           pressure: data.main.pressure,
-          currentTemperature: data.main.temp,
+          currentTemperature: data.main.temp, // Temperature in Celsius
           tempDesc: data.weather[0].description,
         });
+
+        // Pass the user-entered city to the parent component
+        onCityChange(city);
       })
       .catch((error) => {
         console.error('Error fetching current weather data:', error);
       });
-  }, []);
+  };
+
+  // useEffect to fetch weather data when the component initially loads
+  useEffect(() => {
+    fetchWeatherData();
+  }, [city]);
 
   return (
     <div className="ContainerOfTheUpPart">
       <div className="ContainerLeft">
-      <div className="SearchBar">
-                <img src="Images/cloud-computing.png" className="SearchBtn"/>
-                <input type="text" className="SearchInput" placeholder="Enter City Name ..." />
-                <i className="SearchIcon"></i>
-            </div>
-
+        <div className="SearchBar">
+          <img src="Images/cloud-computing.png" className="SearchBtn" onClick={fetchWeatherData} />
+          <input
+            type="text"
+            className="SearchInput"
+            placeholder="Enter City Name ..."
+            value={city}
+            onChange={handleCityChange}
+          />
+          <i className="SearchIcon"></i>
+        </div>
         <div className="Rain">
           <img src="Images/water.png" className="waterImage" />
           <p className="RainDesc">{weatherData.humidity}%</p>
         </div>
-
         <div className="Wind">
           <img src="Images/wind.png" className="windImage" />
-          <p className="WindDesc">{weatherData.wind} km\h</p>
+          <p className="WindDesc">{weatherData.wind} km/h</p>
         </div>
-
         <div className="Pressure">
           <img src="Images/pressure.png" className="pressureImage" />
-          <p className="WindDesc">{weatherData.pressure}p pha</p>
+          <p className="PressureDesc">{weatherData.pressure} hPa</p>
         </div>
       </div>
-
       <div className="ContainerRight">
         <img src="Images/MainSun.png" className="MainImage" />
         <b className="CurrentTemp">{weatherData.currentTemperature}°C</b>
@@ -67,3 +81,4 @@ const CurrentWeather = () => {
 };
 
 export default CurrentWeather;
+        
